b8e7e3782b100e98b455024e3210e9382b6880c6
Your Name <nazik_111@mail.ru>
2019-06-12 19:45:31 +0600

    add "first commit"

---
+++ src/Pallette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Pallette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pallette;
