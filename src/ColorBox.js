83cd543358f4bbb661ecb2020db5ca370b110227
Your Name <nazik_111@mail.ru>
2019-07-01 17:15:51 +0600

    add complete color box refactor

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -59,6 +59,67 @@ const styles = {
     border: 'none',
     textDecoration: 'none',
     opacity: 0
+  },
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    padding: '10px',
+    left: '0px',
+    bottom: '0px',
+    color: 'black',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px'
+  },
+  copyOverlay: {
+    opacity: '0',
+    zIndex: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.6s ease-in-out',
+    transform: 'scale(0.1)'
+  },
+  showOverlay: {
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '10',
+    position: 'absolute'
+  },
+  copyMsg: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '0',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    fontSize: '4rem',
+    transform: 'scale(0.1)',
+    opacity: '0',
+    color: 'white',
+    '& h3': {
+      fontWeight: '400',
+      textShadow: '1px 2px black',
+      background: 'rgba(255, 255, 255, 0.3)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1rem',
+      textTransform: 'uppercase'
+    },
+    '& p': {
+      fontSize: '2rem',
+      fontWeight: '100'
+    }
+  },
+  showCopyMsg: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '25',
+    transition: 'all 0.4s ease-in-out',
+    transitionDelay: '0.3s'
   }
 };
 
@@ -95,14 +156,19 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`copy-overlay ${copied ? 'show' : ''}`}
+            className={`${classes.copyOverlay} ${
+              copied ? classes.showOverlay : ''
+            }`}
           />
-          <div className={`copy-msg ${copied ? 'show' : ''}`}>
+          <div
+            className={`${classes.copyMsg} ${copied &&
+              `${classes.showCopyMsg}`}`}
+          >
             <h3>copied</h3>
             <p className={classes.copyText}>{this.props.background}</p>
           </div>
           <div className="copy-container">
-            <div className="box-content">
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
             <button className={classes.CopyButton}>Copy</button>
