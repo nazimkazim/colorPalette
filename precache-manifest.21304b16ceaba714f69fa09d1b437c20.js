513cbfe15064f47825514c4ae2dab1d7adef3dae
Your Name <nazik_111@mail.ru>
2019-08-19 12:21:19 +0600

    Updates

---
+++ precache-manifest.21304b16ceaba714f69fa09d1b437c20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1db06f5db3c2c727899c875b3327c1",
+    "url": "/colorPalette/index.html"
+  },
+  {
+    "revision": "0e24283db4779448b4ce",
+    "url": "/colorPalette/static/css/2.e8e75e1a.chunk.css"
+  },
+  {
+    "revision": "65a8a958a1d8f0e0dac0",
+    "url": "/colorPalette/static/css/main.61bd9032.chunk.css"
+  },
+  {
+    "revision": "0e24283db4779448b4ce",
+    "url": "/colorPalette/static/js/2.a5a42451.chunk.js"
+  },
+  {
+    "revision": "65a8a958a1d8f0e0dac0",
+    "url": "/colorPalette/static/js/main.61c39cec.chunk.js"
+  },
+  {
+    "revision": "09484d6e51e5e06dc1e1",
+    "url": "/colorPalette/static/js/runtime~main.d471004c.js"
+  },
+  {
+    "revision": "3c903835edd5c6d757f727425302ac95",
+    "url": "/colorPalette/static/media/svg-background.3c903835.svg"
+  }
+]);
